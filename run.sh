1d0120f03e374691c4938896a9e528a33c556514
romanman <roman@ethergit.com>
2015-09-07 12:37:03 -0400

    Fixing shell / bat files

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-set APP_ENV="frontier"
-set APP_ARGS="-Dethereumj.conf.file=$APP_SRC_DIR/main/resources/conf/$APP_ENV.conf"
+export APP_ENV="frontier"
+export APP_ARGS="-Dethereumj.conf.file=$APP_SRC_DIR/main/resources/conf/$APP_ENV.conf"
 
-set JAVA_OPTS="-Xms3g -Xmx4g"
+export JAVA_OPTS="-Xms3g -Xmx4g"
 
 echo "JAVA_OPTS: $JAVA_OPTS"
 echo "APP_ARGS: $APP_ARGS"
