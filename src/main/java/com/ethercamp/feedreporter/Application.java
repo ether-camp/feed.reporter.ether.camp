a623d5296bb90931602dfbea9b41888c6accc81c
romanman <roman@ethergit.com>
2015-09-07 15:53:25 -0400

    Testing

--- src/main/java/com/ethercamp/feedreporter/Application.java
+++ src/main/java/com/ethercamp/feedreporter/Application.java
@@ -1,13 +1,15 @@
 package com.ethercamp.feedreporter;
 
+import com.ethercamp.feedreporter.bean.BeanOne;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-@ComponentScan
+@Configuration
 @SpringBootApplication
 @EnableScheduling
 public class Application {
@@ -15,5 +17,19 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Bean
+    public BeanOne beanOne() {
+
+        System.out.println("  GOOD GOOD GOOD  ");
+        System.out.println("  GOOD GOOD GOOD  ");
+        System.out.println("  GOOD GOOD GOOD  ");
+        System.out.println("  GOOD GOOD GOOD  ");
+        System.out.println("  GOOD GOOD GOOD  ");
+
+        return new BeanOne();
+    }
+
 
 }
