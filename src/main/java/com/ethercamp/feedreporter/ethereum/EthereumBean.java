419639f6727bd64d34a1ebf208ff642d781ed72f
romanman <roman@ethergit.com>
2015-09-07 13:07:03 -0400

    Polishing

--- src/main/java/com/ethercamp/feedreporter/ethereum/EthereumBean.java
+++ src/main/java/com/ethercamp/feedreporter/ethereum/EthereumBean.java
@@ -2,14 +2,25 @@ package com.ethercamp.feedreporter.ethereum;
 
 import org.ethereum.facade.Ethereum;
 import org.ethereum.facade.EthereumFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public class EthereumBean {
+
+
+    private static final Logger log = LoggerFactory.getLogger("general");
 
     Ethereum ethereum;
     EthereumListener ethereumListener;
 
     public void start(){
+
+        log.info(" *                   * ");
+        log.info(" * Starting ethereum * ");
+        log.info(" *                   * ");
+
         this.ethereum = EthereumFactory.createEthereum();
         ethereumListener = new EthereumListener(ethereum);
         this.ethereum.addListener(ethereumListener);
