251a7bcadba62b6fac3c9b9073e5eb67a940f730
romanman <roman@ethergit.com>
2015-09-07 15:44:28 -0400

    Testing

--- src/main/java/com/ethercamp/feedreporter/Application.java
+++ src/main/java/com/ethercamp/feedreporter/Application.java
@@ -7,20 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
+@ComponentScan
 @SpringBootApplication
 @EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
-
-        System.out.println("  GOOD GOOD GOOD  ");
-        System.out.println("  GOOD GOOD GOOD  ");
-        System.out.println("  GOOD GOOD GOOD  ");
-        System.out.println("  GOOD GOOD GOOD  ");
-        System.out.println("  GOOD GOOD GOOD  ");
-
-
-
         SpringApplication.run(Application.class, args);
     }
 
