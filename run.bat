c108ba720011232e34c63269fe5be5ea520d099d
romanman <roman@ethergit.com>
2015-09-07 13:01:41 -0400

    Fix run.bat

--- run.bat
+++ run.bat
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 set APP_ENV="frontier"
-set APP_ARGS="-Dethereumj.conf.file=$APP_SRC_DIR/main/resources/conf/$APP_ENV.conf"
+set APP_ARGS=-Dethereumj.conf.file=%APP_SRC_DIR%/main/resources/conf/%APP_ENV%.conf
 
-set JAVA_OPTS="-Xms3g -Xmx4g"
 
 echo "JAVA_OPTS: %JAVA_OPTS%"
 echo "APP_ARGS: %APP_ARGS%"
 
-./gradlew clean bootRun -PjvmArgs="%JAVA_OPTS%" %APP_ARGS% > out.log
+gradlew clean bootRun -PjvmArgs="-Xms3g -Xmx4g" %APP_ARGS% > out.log
